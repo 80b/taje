e160653e9f946f89ef4966a884b21e41726603c4
Tyty <54728263+80b@users.noreply.github.com>
2020-11-15 00:34:22 -0500

    Update setup.py

--- Source/Setup/setup.py
+++ Source/Setup/setup.py
@@ -1,1 +1,18 @@
+import os
+print("Setting up Taje...")
+yorn = input("Would you like Taje to ping a role when someone uses it? Y/N: ")
 
+
+if yorn == "Y":
+	setup = open("PingFile.txt", "x")
+	setup_file = open("PingFile.txt", "w")
+	pingme = input("What is your id? Format: <@YOURID>: ")
+	setup_file.write(pingme)
+	setup_file.close()
+	print("Done!")
+else:
+	pass
+
+print("Installing requirements...")
+os.system("pip install discord-webhook")
+print("Done.")
