016d5afbe62986a2c699c60ad917f64a2fb55410
Tyty <54728263+80b@users.noreply.github.com>
2020-11-15 00:49:25 -0500

    Update setup.py

--- Source/Setup/setup.py
+++ Source/Setup/setup.py
@@ -4,9 +4,10 @@ yorn = input("Would you like Taje to ping a role when someone uses it? Y/N: ")
 
 
 if yorn == "Y":
-	setup = open("PingFile.txt", "x")
-	setup_file = open("PingFile.txt", "w")
+	
+	setup_file = open("PingFile.txt", "w+")
 	pingme = input("What is your id? Format: <@YOURID>: ")
+	
 	setup_file.write(pingme)
 	setup_file.close()
 	print("Done!")
